be9bc8e43e10e7320dfc4e97b38d8b6d5884d552
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-07-24 00:51:49 +0800

    led

--- led.Designer.vb
+++ led.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class led
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,35 +20,173 @@ Partial Class led
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Back = New System.Windows.Forms.Button()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.minus_btn = New System.Windows.Forms.Button()
+        Me.plus_btn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.addtocart_btn = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Panel1
+        'Back
         '
-        Me.Panel1.AutoScroll = True
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(904, 611)
-        Me.Panel1.TabIndex = 0
+        Me.Back.BackColor = System.Drawing.Color.Transparent
+        Me.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Back.Font = New System.Drawing.Font("Microsoft Himalaya", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Back.ForeColor = System.Drawing.Color.Black
+        Me.Back.Location = New System.Drawing.Point(773, 11)
+        Me.Back.Margin = New System.Windows.Forms.Padding(2)
+        Me.Back.Name = "Back"
+        Me.Back.Size = New System.Drawing.Size(103, 31)
+        Me.Back.TabIndex = 88
+        Me.Back.Text = "BACK"
+        Me.Back.UseVisualStyleBackColor = False
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Font = New System.Drawing.Font("Microsoft Sans Serif", 29.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label22.Location = New System.Drawing.Point(36, 11)
+        Me.Label22.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(119, 44)
+        Me.Label22.TabIndex = 87
+        Me.Label22.Text = "LEDs"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(537, 344)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(181, 37)
+        Me.Label4.TabIndex = 108
+        Me.Label4.Text = "PHP 25.00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(537, 387)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 20)
+        Me.Label3.TabIndex = 107
+        Me.Label3.Text = "Quantity"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 29.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(530, 107)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(200, 88)
+        Me.Label2.TabIndex = 106
+        Me.Label2.Text = "LED 5mm" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(10 pcs)"
+        '
+        'minus_btn
+        '
+        Me.minus_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.minus_btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minus_btn.Location = New System.Drawing.Point(789, 410)
+        Me.minus_btn.Margin = New System.Windows.Forms.Padding(2)
+        Me.minus_btn.Name = "minus_btn"
+        Me.minus_btn.Size = New System.Drawing.Size(47, 36)
+        Me.minus_btn.TabIndex = 105
+        Me.minus_btn.Text = "-"
+        Me.minus_btn.UseVisualStyleBackColor = True
+        '
+        'plus_btn
+        '
+        Me.plus_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.plus_btn.Location = New System.Drawing.Point(737, 410)
+        Me.plus_btn.Margin = New System.Windows.Forms.Padding(2)
+        Me.plus_btn.Name = "plus_btn"
+        Me.plus_btn.Size = New System.Drawing.Size(47, 36)
+        Me.plus_btn.TabIndex = 104
+        Me.plus_btn.Text = "+"
+        Me.plus_btn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(538, 410)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(195, 35)
+        Me.TextBox1.TabIndex = 103
+        '
+        'addtocart_btn
+        '
+        Me.addtocart_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.addtocart_btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addtocart_btn.Location = New System.Drawing.Point(538, 459)
+        Me.addtocart_btn.Margin = New System.Windows.Forms.Padding(2)
+        Me.addtocart_btn.Name = "addtocart_btn"
+        Me.addtocart_btn.Size = New System.Drawing.Size(298, 54)
+        Me.addtocart_btn.TabIndex = 102
+        Me.addtocart_btn.Text = "ADD TO CART"
+        Me.addtocart_btn.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.Logic_Sheeshcuit.My.Resources.Resources.Product_7
+        Me.PictureBox1.Location = New System.Drawing.Point(44, 107)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(450, 407)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 101
+        Me.PictureBox1.TabStop = False
         '
         'led
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
         Me.ClientSize = New System.Drawing.Size(904, 611)
         Me.ControlBox = False
-        Me.Controls.Add(Me.Panel1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.minus_btn)
+        Me.Controls.Add(Me.plus_btn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.addtocart_btn)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Back)
+        Me.Controls.Add(Me.Label22)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "led"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "LEDs"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Back As Button
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents minus_btn As Button
+    Friend WithEvents plus_btn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents addtocart_btn As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
