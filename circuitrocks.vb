e0c19b8893e15a4649573c98241239d62dcf1636
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-07-24 14:36:36 +0800

    circuitrocks

--- circuitrocks.vb
+++ circuitrocks.vb
@@ -1,3 +1,10 @@
 ﻿Public Class circuitrocks
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        home.Show()
+    End Sub
 
+    Private Sub circuitrocks_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
