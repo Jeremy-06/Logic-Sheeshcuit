17fcd588f8af129528968b2e7c0882305fa45563
Lilithzie <queenlilithzie@gmail.com>
2025-07-27 16:46:44 +0800

    Oscillos The Copes :D

--- oscilloscopes.Designer.vb
+++ oscilloscopes.Designer.vb
@@ -26,8 +26,34 @@ Partial Class oscilloscopes
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.minus_btn = New System.Windows.Forms.Button()
+        Me.plus_btn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.addtocart_btn = New System.Windows.Forms.Button()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.minus_btn1 = New System.Windows.Forms.Button()
+        Me.plus_btn1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.addtocart_btn1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.Label30 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -80,6 +106,254 @@ Partial Class oscilloscopes
         Me.Label1.TabIndex = 203
         Me.Label1.Text = "Oscilloscopes"
         '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(524, 224)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(337, 63)
+        Me.Label5.TabIndex = 213
+        Me.Label5.Text = "All-in-one oscilloscope and digital multimeter." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- Compact and portable" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- Useful" &
+    " for engineers and fieldwork"
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.Gainsboro
+        Me.PictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox9.Location = New System.Drawing.Point(528, 298)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(12, 41)
+        Me.PictureBox9.TabIndex = 212
+        Me.PictureBox9.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.Font = New System.Drawing.Font("Calibri", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(539, 298)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(167, 41)
+        Me.Label4.TabIndex = 210
+        Me.Label4.Text = "₱16,750.00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Gainsboro
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(655, 369)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 19)
+        Me.Label3.TabIndex = 209
+        Me.Label3.Text = "QUANTITY"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Bahnschrift SemiBold Condensed", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(518, 102)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(321, 122)
+        Me.Label2.TabIndex = 208
+        Me.Label2.Text = "Oscilloscope Handheld DMM"
+        '
+        'minus_btn
+        '
+        Me.minus_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.minus_btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minus_btn.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.minus_btn.Location = New System.Drawing.Point(539, 392)
+        Me.minus_btn.Margin = New System.Windows.Forms.Padding(2)
+        Me.minus_btn.Name = "minus_btn"
+        Me.minus_btn.Size = New System.Drawing.Size(47, 36)
+        Me.minus_btn.TabIndex = 207
+        Me.minus_btn.Text = "-"
+        Me.minus_btn.UseVisualStyleBackColor = True
+        '
+        'plus_btn
+        '
+        Me.plus_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.plus_btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plus_btn.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.plus_btn.Location = New System.Drawing.Point(796, 392)
+        Me.plus_btn.Margin = New System.Windows.Forms.Padding(2)
+        Me.plus_btn.Name = "plus_btn"
+        Me.plus_btn.Size = New System.Drawing.Size(47, 36)
+        Me.plus_btn.TabIndex = 206
+        Me.plus_btn.Text = "+"
+        Me.plus_btn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(590, 393)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(205, 35)
+        Me.TextBox1.TabIndex = 205
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'addtocart_btn
+        '
+        Me.addtocart_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.addtocart_btn.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addtocart_btn.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.addtocart_btn.Location = New System.Drawing.Point(539, 440)
+        Me.addtocart_btn.Margin = New System.Windows.Forms.Padding(2)
+        Me.addtocart_btn.Name = "addtocart_btn"
+        Me.addtocart_btn.Size = New System.Drawing.Size(307, 54)
+        Me.addtocart_btn.TabIndex = 204
+        Me.addtocart_btn.Text = "ADD TO CART"
+        Me.addtocart_btn.UseVisualStyleBackColor = True
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.Gainsboro
+        Me.PictureBox8.Location = New System.Drawing.Point(524, 360)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(337, 149)
+        Me.PictureBox8.TabIndex = 211
+        Me.PictureBox8.TabStop = False
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(711, 751)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(171, 45)
+        Me.Label10.TabIndex = 224
+        Me.Label10.Text = "- High-resolution screen" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- USB rechargeable" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "    "
+        '
+        'minus_btn1
+        '
+        Me.minus_btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.minus_btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minus_btn1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.minus_btn1.Location = New System.Drawing.Point(541, 844)
+        Me.minus_btn1.Margin = New System.Windows.Forms.Padding(2)
+        Me.minus_btn1.Name = "minus_btn1"
+        Me.minus_btn1.Size = New System.Drawing.Size(47, 36)
+        Me.minus_btn1.TabIndex = 217
+        Me.minus_btn1.Text = "-"
+        Me.minus_btn1.UseVisualStyleBackColor = True
+        '
+        'plus_btn1
+        '
+        Me.plus_btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.plus_btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plus_btn1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.plus_btn1.Location = New System.Drawing.Point(801, 844)
+        Me.plus_btn1.Margin = New System.Windows.Forms.Padding(2)
+        Me.plus_btn1.Name = "plus_btn1"
+        Me.plus_btn1.Size = New System.Drawing.Size(47, 36)
+        Me.plus_btn1.TabIndex = 216
+        Me.plus_btn1.Text = "+"
+        Me.plus_btn1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(592, 845)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(205, 35)
+        Me.TextBox2.TabIndex = 215
+        '
+        'addtocart_btn1
+        '
+        Me.addtocart_btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.addtocart_btn1.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addtocart_btn1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.addtocart_btn1.Location = New System.Drawing.Point(541, 892)
+        Me.addtocart_btn1.Margin = New System.Windows.Forms.Padding(2)
+        Me.addtocart_btn1.Name = "addtocart_btn1"
+        Me.addtocart_btn1.Size = New System.Drawing.Size(307, 54)
+        Me.addtocart_btn1.TabIndex = 214
+        Me.addtocart_btn1.Text = "ADD TO CART"
+        Me.addtocart_btn1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(629, 683)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(217, 54)
+        Me.Label6.TabIndex = 223
+        Me.Label6.Text = "Dual channel for comparing two signals." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "    "
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.BackColor = System.Drawing.Color.Gainsboro
+        Me.PictureBox10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox10.Location = New System.Drawing.Point(528, 751)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(12, 41)
+        Me.PictureBox10.TabIndex = 222
+        Me.PictureBox10.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label7.Font = New System.Drawing.Font("Calibri", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(539, 751)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(167, 41)
+        Me.Label7.TabIndex = 220
+        Me.Label7.Text = "₱13,750.00"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Gainsboro
+        Me.Label8.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(655, 820)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(79, 19)
+        Me.Label8.TabIndex = 219
+        Me.Label8.Text = "QUANTITY"
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Bahnschrift SemiBold Condensed", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(518, 556)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(354, 180)
+        Me.Label9.TabIndex = 218
+        Me.Label9.Text = "Oscilloscope with 2-Channel Handheld DMM"
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.BackColor = System.Drawing.Color.Gainsboro
+        Me.PictureBox11.Location = New System.Drawing.Point(524, 812)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(337, 149)
+        Me.PictureBox11.TabIndex = 221
+        Me.PictureBox11.TabStop = False
+        '
+        'Label30
+        '
+        Me.Label30.Location = New System.Drawing.Point(381, 965)
+        Me.Label30.Name = "Label30"
+        Me.Label30.Size = New System.Drawing.Size(100, 43)
+        Me.Label30.TabIndex = 225
+        '
         'oscilloscopes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -87,6 +361,28 @@ Partial Class oscilloscopes
         Me.AutoScroll = True
         Me.ClientSize = New System.Drawing.Size(904, 611)
         Me.ControlBox = False
+        Me.Controls.Add(Me.Label30)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.minus_btn1)
+        Me.Controls.Add(Me.plus_btn1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.addtocart_btn1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.minus_btn)
+        Me.Controls.Add(Me.plus_btn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.addtocart_btn)
+        Me.Controls.Add(Me.PictureBox8)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
@@ -98,6 +394,10 @@ Partial Class oscilloscopes
         Me.Text = "Oscilloscopes"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -106,4 +406,26 @@ Partial Class oscilloscopes
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents minus_btn As Button
+    Friend WithEvents plus_btn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents addtocart_btn As Button
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents minus_btn1 As Button
+    Friend WithEvents plus_btn1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents addtocart_btn1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents Label30 As Label
 End Class
